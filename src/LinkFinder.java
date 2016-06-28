1ccfae850d1408d314d21a1bb001a99b23dc7e02
ghovseth <ghovseth@student.neumont.edu>
2016-06-28 14:35:52 -0600

    added pattern and iterator

--- src/LinkFinder.java
+++ src/LinkFinder.java
@@ -4,20 +4,36 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class LinkFinder {//a href=".*"
+public class LinkFinder {//<a href=".*">
 	
 	private File file = new File("C:\\Users\\Gavin\\Documents\\4th Quarter\\Platform Dev\\results");
 	private FileInputStream stream;
+	public ArrayList<String> links;
 	
 	public void processPage(InputStream in){
 		BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+		try{
+			String value = reader.readLine();
+			String pattern = new String("<a\\shref=\"(.*)\">");
+			Pattern p = Pattern.compile(pattern);
+			Matcher m = p.matcher(value);
+			if(m.matches()){
+				String save = m.group(1);
+				links.add(save);
+			}
+		}
+		catch(IOException e){
+			e.printStackTrace();
+		}
 	}
 	
 	public Iterator <String> getLinks(){
-		return null;
-		
+		return links.iterator();
 	}
 	
 	public void makeInputStream(){
