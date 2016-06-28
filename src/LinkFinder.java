ee00f5fea3abc8e8af0f87048c5b895b79cfb8f2
ghovseth <ghovseth@student.neumont.edu>
2016-06-27 21:22:10 -0600

    added buffered reader and InputStream

--- src/LinkFinder.java
+++ src/LinkFinder.java
@@ -1,14 +1,34 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Iterator;
 
-public class LinkFinder {
+public class LinkFinder {//a href=".*"
+	
+	private File file = new File("C:\\Users\\Gavin\\Documents\\4th Quarter\\Platform Dev\\results");
+	private FileInputStream stream;
 	
 	public void processPage(InputStream in){
-		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(in));
 	}
 	
 	public Iterator <String> getLinks(){
 		return null;
 		
+	}
+	
+	public void makeInputStream(){
+		try{
+			stream = new FileInputStream(file);
+		}
+		catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+	public FileInputStream getStream(){
+		return stream;
 	}
 }
